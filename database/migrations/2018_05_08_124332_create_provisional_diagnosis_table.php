d9baad51720d6d83a3e5a79c672e03eb6a1310a1
Mital28 <mital.sharma@netfonia.us>
2018-05-08 16:06:17 +0530

    prvisionaldiagnisistable

---
+++ database/migrations/2018_05_08_124332_create_provisional_diagnosis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvisionalDiagnosisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provisional_diagnosis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('category_id')->nullable();
+            $table->enum('status', [0, 1])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provisional_diagnosis');
+    }
+}
