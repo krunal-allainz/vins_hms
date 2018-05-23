0bdbd0ab8a216bc618fdfd5ccb5faefd4c8117c2
Mital28 <mital.sharma@netfonia.us>
2018-05-23 16:03:52 +0530

    add seeder class and remove npm print pakage

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
          $this->call(roleUserSeeder::class);
          $this->call(DiagnosisCategoryTableSeeder::class);
          $this->call(ProvisionalDiagnosistableSeeder::class);
+          $this->call(PrescriptionDrugsTableSeeder::class);
          DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
     }
