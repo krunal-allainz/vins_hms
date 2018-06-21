1dc269feee9848d533855d8125b8f6a25490263b
Richa Pathak <Kp@gmail.com>
2018-06-21 14:53:12 +0530

    database seeder file

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -269,7 +269,28 @@ class UsersTableSeeder extends Seeder
 
         ]);
         
-        
+        DB::table('users')->insert([
+            // ['person_id' => '3','username' => 'tadministrator@administrator.com',
+            ['first_name'=>'Dr. RAKESH',
+            'password'=>Hash::make(trim('12345678')),
+            'department'=>'Neurology',
+            'user_type'=>'Doctor',
+            'mobile_no'=>'123456789',
+            'address'=>'Vadodara',
+            'user_image'=>NULL,
+            'token'=>'',
+            'is_verified'=>'1',
+            'timezone'=>NULL,
+            'last_login_time'=>NULL,
+            'is_active'=>'1',
+            'last_active_time'=>NULL,
+            'is_mobile_user'=>1,
+            'deleted_at'=>NULL,
+            'last_name'=>'SHAH',
+            'email'=>'rakesh.shah@gmail.com',
+            'status'=>'Active'],
+
+        ]);
         
 
 
