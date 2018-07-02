2a2665f88ade3ac89c7c288716f987b0a9c07748
Mital28 <mital.sharma@netfonia.us>
2018-07-02 16:01:02 +0530

    new template add

--- resources/assets/js/views/admin/vins/dashboard.vue
+++ resources/assets/js/views/admin/vins/dashboard.vue
@@ -1,42 +1,97 @@
 <template>
+	 <section class="content">
+		<div class="row">
+                <div class="col-sm-6 col-md-6 col-xl-3">
+                    <div class="flip">
+                        <div class="widget-bg-color-icon card-box front">
+                            <div class="bg-icon float-left">
+                                <i class="ti-eye text-warning"></i>
+                            </div>
+                            <div class="text-right">
+                                <h3 class="text-dark"><b>3752</b></h3>
+                                <p>Daily Visits</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="widget-bg-color-icon card-box back">
+                            <div class="text-center">
+                                <span id="loadspark-chart"></span>
+                                <hr>
+                                <p>Check summary</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-xl-3">
+                    <div class="flip">
+                        <div class="widget-bg-color-icon card-box front">
+                            <div class="bg-icon float-left">
+                                <i class="ti-shopping-cart text-success"></i>
+                            </div>
+                            <div class="text-right">
+                                <h3><b id="widget_count3">3251</b></h3>
+                                <p>Sales status</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="widget-bg-color-icon card-box back">
+                            <div class="text-center">
+                                <span class="linechart" id="salesspark-chart"></span>
+                                <hr>
+                                <p>Check summary</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
 
-  <div class="container">
-  	<div>
-  	</div>
- </div>
+                <div class="col-sm-6 col-md-6 col-xl-3">
+                    <div class="flip">
+                        <div class="widget-bg-color-icon card-box front">
+                            <div class="bg-icon float-left">
+                                <i class="ti-thumb-up text-danger"></i>
+                            </div>
+                            <div class="text-right">
+                                <h3 class="text-dark"><b>1532</b></h3>
+                                <p>Hits</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="widget-bg-color-icon card-box back">
+                            <div class="text-center">
+                                <span id="visitsspark-chart"></span>
+                                <hr>
+                                <p>Check summary</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-xl-3">
+                    <div class="flip">
+                        <div class="widget-bg-color-icon card-box front">
+                            <div class="bg-icon float-left">
+                                <i class="ti-user text-info"></i>
+                            </div>
+                            <div class="text-right">
+                                <h3 class="text-dark"><b>1252</b></h3>
+                                <p>Subscribers</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="widget-bg-color-icon card-box back">
+                            <div class="text-center">
+                                <span id="subscribers-chart"></span>
+                                <hr>
+                                <p>Check summary</p>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
 </template>
-
-<script >
-    export default {
-        data() {
-            return {
-                'footer' : 'footer',
-                'currentYear': new Date().getFullYear(),
-                'userType': this.$store.state.Users.userDetails.user_type
-            }
-        },
-        mounted() {
-        	let vm =this;
-        	$("body .js-loader").removeClass('d-none');
-        	// setTimeout(function(){
-        		if(vm.userType == 'Doctor'){
-                		vm.$router.push({'name':'opd_form'});
-	        	} else if(vm.userType == 'Others'){
-                        vm.$router.push({'name':'patients_detail_form'});
-	        		// vm.$router.push('nabhReport');
-	        	}
-	        	
-	        	$("body .js-loader").addClass('d-none');
-        	// },500)
-        	
-        	// this.$store.dispatch('SetIpdId',0);
-
-    },
-        methods: {
-		    GetSelectComponent(componentName) {
-		       this.$router.push({name: componentName})
-		    }
-		  },
-    }
-</script>
