eb23c1129acad4290beb1e10f5aa53d62bb38b89
Mital28 <mital.sharma@netfonia.us>
2018-08-02 17:04:10 +0530

    add decimal validation

--- resources/assets/js/views/admin/vins/opd_form.vue
+++ resources/assets/js/views/admin/vins/opd_form.vue
@@ -183,7 +183,7 @@
                     </div>
                     <div class="col-md-6">
                       <div class=" input-group">
-                      <input type="text" name="temp" id="temp" class="form-control"  v-model="opdData.temp" v-validate="'required|numeric|min_value:1'" maxlength="3">
+                      <input type="text" name="temp" id="temp" class="form-control"  v-model="opdData.temp" v-validate="'required|numeric|min_value:1'" pattern="\d{1,3}(\.\d{0,1})?" >
                         <div class="input-group-append">
                             <span class="input-group-text ">°F</span>
                         </div>
