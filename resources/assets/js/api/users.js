a8f4cef5b0d51f069c5c9e412cd4a7a3040667cd
Mital28 <mital.sharma@netfonia.us>
2018-08-09 10:36:43 +0530

    working files

--- resources/assets/js/api/users.js
+++ resources/assets/js/api/users.js
@@ -325,6 +325,9 @@ export default {
      return api.post('user/user_exist',{'type': type , value : value});
   },
   getLastOPDIdByPatientId(patient_id) {
-    return api.post('patient/getLastOPDIdByPatientId',{'patient_id':patient_id})
+    return api.post('patient/getLastOPDIdByPatientId',{'patient_id':patient_id});
   },
+  getAllPatientList(page_url,type,noofRecord) { 
+      return api.post(page_url,{'type':type,'noOfRecord':noofRecord});
+  }
 }
---
+++ resources/assets/js/views/admin/vins/patientDetailInfo.vue
@@ -0,0 +1,14 @@
+<!-- template for the modal component -->
+    <transition name="modal">
+        <div class="modal-mask" @click="close" v-show="show">
+            <div class="modal-container" @click.stop>
+              <slot></slot>
+            </div>
+        </div>
+    </transition>
+
+
+
+<script >
+
+</script>
