b162b2a2147f8e546f71ab14ca660e1f1fbcb5d4
Richa <richa@gmail.com>
2018-08-21 11:18:56 +0530

    print button hide

--- resources/assets/js/views/admin/vins/opd_form_thankyou.vue
+++ resources/assets/js/views/admin/vins/opd_form_thankyou.vue
@@ -53,6 +53,7 @@
 
 					<!-- <button type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal"  v-show="(checkedreportList.length != 0)" @click = "printReport('opd_case')" >OPD Case</button> -->
 					<!-- <button type="button" class="btn btn-primary btn-submit text-right" @click="print_multiple_report()">Print</button> -->
+					
 
 					<button type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal"  v-show="(checkedreportList.length != 0)" @click = "printReport('opd_case')" >OPD Case</button>
 					<!-- <button ty pe="button" lass="btn btn-primary btn-submit text-right" >Print</button> -->
