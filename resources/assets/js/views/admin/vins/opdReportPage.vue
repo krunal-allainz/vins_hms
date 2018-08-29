b1c26fe41b94c8b486f1d712836cbb158249dd68
Mital28 <mital.sharma@netfonia.us>
2018-08-29 12:58:16 +0530

    working files

--- resources/assets/js/views/admin/vins/opdReportPage.vue
+++ resources/assets/js/views/admin/vins/opdReportPage.vue
@@ -13,12 +13,27 @@
     	 	 		<div class="col-md-12">
     	 	 			<button type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#generateModal"  @click="printReport('generate_case')" v-if="(opdReport == false)"> Print Report</button>
     	 	 			<button type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal"  @click = "printReport('print_perceptions')"  id="opd_case_btn">Print Perceptions</button>
-    	 	 			<button  v-if="(labReportData)" type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('lab')" >Lab Report</button>
-    	 	 			<button  v-if="(radioReportData)" type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('radiology')" >Radiology Report</button>
-    	 	 			<button v-if="(prescriptiData)" type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('prescription')">Print Prescription</button>
+    	 	 			<button   type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('lab')" >Lab Report</button>
+    	 	 			<button   type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('radiology')" >Radiology Report</button>
+    	 	 			<button  type="button" class="btn btn-primary btn-submit text-right" data-toggle="modal" data-backdrop="static" href="#printModal" @click = "printReport('prescription')">Print Prescription</button>
     	 	 		</div>
-    	 	 	</div>s
+    	 	 	</div>
     		</form>
+    		<div id="printModal" class="modal fade">
+     			<div class="modal-dialog" >
+		 			<div class="modal-content" >
+		 			<!--<div class="modal-header"></div>-->
+		 			<div class="modal-body">
+			 			<opdReportView :opdId="opdId" patinetId="patinetId" :todayDate="todayDate" :patientCheckupDetail="patientCheckupDetail" :signatureName="signatureName" :doctoreName="doctoreName" :regNo="regNo" :patientDetail="patientDetail" :department="department" :timeStamp="timeStamp" :reference="reference"></opdReportView>
+			 		</div>
+			 		<div class="modal-footer">	
+						<button  type="button" class="btn btn-primary"  @click="ClickHereToPrint()">Print</button>	
+               			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+             			<!--  <button type="button" class="btn btn-primary">Save</button>-->
+             		</div>	
+					</div>
+				</div>
+			</div>
     	</div>
 	</div>
 </template>
@@ -26,10 +41,7 @@
 	import User from '../../../api/users.js';
 	import vinsletterheadheaderpart from './vins_letter_header.vue';
 	import vinsletterheadfooterpart from './vins_letter_footer.vue';
-	import patientDetailReport from './patientDetailReport.vue';
-	import patientCheckupReport from './patientCheckupReport.vue';
-	import prescriptionData from './prescriptionData.vue';
-	import prescriptionPrint from './prescriptionPrint.vue';
+	import opdReportView from './opdReportView.vue';	import prescriptionPrint from './prescriptionPrint.vue';
 	import moment from 'moment';
 	import print from 'print-js'
 	import _ from 'lodash';
@@ -53,14 +65,12 @@
 				'patientDetail' : {},
 		      	'patientCheckupDetail' : {},
 		      	'department': '',
+		      	'reference' : '',
 			}
 		},
 		components: {
-         vinsletterheadheaderpart,
-         vinsletterheadfooterpart,
-         prescriptionPrint,
-         patientDetailReport,
-         patientCheckupReport,
+         
+         opdReportView,
        },
        mounted(){
        	let vm =this;
--- resources/assets/js/views/admin/vins/opdReportView.vue
+++ resources/assets/js/views/admin/vins/opdReportView.vue
@@ -1,4 +1,19 @@
 <template>
+	<div>
+		<div id="printData" style="height: 1500px;min-height: 700px;">
+			 			<div  id="printContent"></div>
+			<div>
+				<vinsletterheadheaderpart></vinsletterheadheaderpart>
+				<patientDetailReport :patientDetail="patientDetail" :consult_dr="doctoreName" :todayDate="todayDate"  :department="department" :reg_no="regNo" :refer_dr="reference">
+				</patientDetailReport>
+			 	<patientCheckupReport :patientCheckupDetail="patientCheckupDetail">
+			 	</patientCheckupReport>
+				<vinsletterheadfooterpart></vinsletterheadfooterpart>
+			</div>
+		</div>
+		
+	</div>
+
 </template>
 <script>
 	import vinsletterheadheaderpart from './vins_letter_header.vue';
@@ -11,24 +26,14 @@
 	import _ from 'lodash';
 	
 	export default {
-		props: ['opdId','patinetId','todayDate'],
+		props: ['opdId','patinetId','todayDate','patientDetail','patientCheckupDetail','department','regNo','doctoreName','signatureName','consultntId','reference'],
 		data() {
 			return{
-				'consultntId' : '',
-				'consultName' : '',
-				'signatureName' : '',
-				'doctoreName' :'',
-				'timeStamp' : '',
-				'regNo':'',
-				'patientDetail' : {},
-		      	'patientCheckupDetail' : {},
-		      	'department': '',			
+			
 		      }
 		},
 		 mounted(){
        	let vm =this;
-       	vm.getOpdData(vm.opdId);
-       	vm.getPatientData(vm.patinetId);
        },
 		components: {
          vinsletterheadheaderpart,
@@ -38,94 +43,9 @@
          patientCheckupReport,
        },
         methods: {
-       		getOpdData(opdId){
-       			User.getPatientOpdData().then(
-       				(response) => {
-       					console.log(response.data.data);
-       				},
-       				(error) => {
-       				}
-       			);
-       		},
-       		getPatientData(patinetId)
-			{
-				var vm=this;
-				User.generatePatientDetailsByID(patinetId).then(
-	  				(response) => {
-	  					
-	  					if(response.data.code == 200){
-	  						//console.log(response.data.data);
-	  						vm.patientDetail = response.data.data;
-	  					}
-	  				},
-	  				(error) => {
-
-	  				}
-  				);
-  				User.getOPDDetailsByPatientId(patinetId).then(
-  					(response) => {
-	  					
-	  					if(response.data.code == 200){
-	  						vm.opdId = response.data.data.id;
-	  						vm.sectionOpdId = response.data.data.opd_id;
-	  						User.generatePatientCheckUpDetails(vm.opdId).then(
-  							(response) => {
-	  							if(response.data.code == 200){
-	  							vm.patientCheckupDetail = response.data.data;
-	  							}
-	  							var sectionOpdId = vm.sectionOpdId;
-			  				User.getPatientCaseDetailByOpdId(sectionOpdId).then(
-			  					(response) => {
-			  					console.log(response.data.data);
-				  					if(response.data.code == 200){
-				  					vm.reference = response.data.data.references;
-				  					vm.consultntId = response.data.data.consultant_id;
-				  					}
-				  					var userType = 1;
-						  			User.getDoctoreInfoById(vm.consultntId,userType).then(
-						  				(response) => {
-						  					if(response.data.code == 200){
-						  						var data = response.data.data;
-						  						//vm.signatureName = data.name;
-						  						vm.doctoreName = data.name;
-						  						vm.timeStamp =data.dagree;  
-						  						vm.department =data.department;	
-						  						vm.regNo =data.regNo;
-						  					}
-						  				},
-						  				(error) => {
-
-						  				}
-						  			);
-						  			User.generateUserNameById(vm.consultntId).then(
-						  				(response) => {
-											vm.consultName = response.data;
-											vm.signatureName = response.data;
-										},
-									    (error) => {
-									    },
-						  			); 
-  			
-				  				},
-				  				(error) => {
-
-				  				}
-			  					);
-	  						},
-	  						(error) => {
-
-	  							}
-
-  							);
-	  					}
-	  				},
-	  				(error) => {
-
-	  				}
-  				);
-			},
 			GetSelectComponent(componentName) {
 		       this.$router.push({name: componentName})
 		   	}
        }
+   }
 </script>
