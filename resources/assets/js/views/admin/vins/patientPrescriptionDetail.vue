849477abf84a455a18290017d3a94a39f412b98a
Mital28 <mital.sharma@netfonia.us>
2018-09-21 18:28:05 +0530

    prescription look chnages

--- resources/assets/js/views/admin/vins/patientPrescriptionDetail.vue
+++ resources/assets/js/views/admin/vins/patientPrescriptionDetail.vue
@@ -5,28 +5,25 @@
                 <div class="col-md-12">
                   <div class="table-responsive">
                     <table class="table report_table"  v-if="setPresPrint" id="">
-                      
                         <tbody v-if="presp_count(prescriptData)>0">
-                        
                          <tr v-for="(res,index) in prescriptData" v-if="res.remove=='false'" :id="res.pid">
                                     
                                 <td>  {{++index}}] {{res.name}} :ORAL
                                     
-                                    <span v-if="res.type=='Q-Hrs'">
+                                    <span v-if="res.how_many_times=='Q-Hrs'">
                                         <span v-for="(item,index) in parseInt(res.total_qhrs)">
-
                                             <span v-if="index!=0 && res['clock_quantity_'+(index+1)]!='0'">
                                                <span v-if=""> ______</span>
                                             </span>
-                                            <span v-if="res['clock_quantity_'+(index+1)]!='0'"> {{res['clock_quantity_'+(index+1)]}}</span>
+                                            <span v-if="res['clock_quantity_'+(index+1)]!='0'"> {{res['clock_quantity_'+(index+1)]}} {{</span>
                                         </span>
                                     </span>
                                     <span v-else>
-                                        <span v-if="res.clock_quantity_1!='0'"> {{res.clock_quantity_1}}</span>
-                                        <span v-if="res.clock_quantity_2!='0'"> <span v-if="res.clock_quantity_1!='0'"> ______</span>{{res.clock_quantity_2}}</span><span v-if="res.clock_quantity_3!='0'"> <span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0'"> ______</span>{{res.clock_quantity_3}}</span><span v-if="res.clock_quantity_4!='0'"><span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0' || res.clock_quantity_3!='0'">  ______</span>{{res.clock_quantity_4}}</span>
+                                        <span v-for="clockinfo in prescriptionclockDetail[res.preId]">{{clockinfo.clock_quantity}}<span>______</span></span>
+                                       <!--  <span v-if="res.clock_quantity_1!='0'"> {{res.clock_quantity_1}}</span>
+                                        <span v-if="res.clock_quantity_2!='0'"> <span v-if="res.clock_quantity_1!='0'"> ______</span>{{res.clock_quantity_2}}</span><span v-if="res.clock_quantity_3!='0'"> <span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0'"> ______</span>{{res.clock_quantity_3}}</span><span v-if="res.clock_quantity_4!='0'"><span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0' || res.clock_quantity_3!='0'">  ______</span>{{res.clock_quantity_4}}</span> -->
                                     </span>
-                                    
-                                    <span v-if="res.type=='Q-Hrs'">
+                                    <span v-if="res.how_many_times=='Q-Hrs'">
                                         [
                                          <span v-for="(item,index) in parseInt(res.total_qhrs)">
                                             <span v-if="index!=0 && res['clock_time_'+(index+1)]!='0' && res['clock_time_'+(index+1)]!='--:--'">
@@ -37,12 +34,12 @@
                                         ]
                                     </span>
                                     <span v-else>
-                                        <span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0' || res.clock_quantity_3!='0' || res.clock_quantity_4!='0'">
+                                       <span v-for="clockinfo in prescriptionclockDetail[res.preId]">{{clockinfo.clock_time}}<span>______</span></span>
+                                       <!--  <span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0' || res.clock_quantity_3!='0' || res.clock_quantity_4!='0'">
                                          [ <span v-if="res.clock_quantity_1!='0'"> {{res.clock_time_1}}</span><span v-if="res.clock_quantity_2!='0'"><span v-if="res.clock_quantity_1!='0'"> ______</span> {{res.clock_time_2}}</span><span v-if="res.clock_quantity_3!='0'"><span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0'"> ______</span> {{res.clock_time_3}}</span><span v-if="res.clock_quantity_4!='0'"><span v-if="res.clock_quantity_1!='0' || res.clock_quantity_2!='0' || res.clock_quantity_3!='0'"> ______</span>{{res.clock_time_4}}</span> ] 
-                                    </span>
+                                    </span> -->
                                     </span>
                                     
-                                  
                                     <span v-if="(res.clock_suggest!='' && res.clock_suggest!='--')">
                                         [ <span>{{res.clock_suggest}}</span> ]
                                     </span>
@@ -69,7 +66,7 @@
 </template>
 <script>
 	export default {
-		props:['prescriptData'],
+		props:['prescriptData','prescriptionclockDetail'],
      data() {
             return {
                'notValid':false,
