e2f1f35e59e317501067896d0bc868bcd114ee8c
Mital28 <mital.sharma@netfonia.us>
2018-09-27 18:04:18 +0530

    add migration for timeline task

---
+++ database/migrations/2018_09_27_173250_create_notifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications_timeline', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->string('type')->nullable();
+            $table->string('data_table')->nullable();
+            $table->integer('data_id')->nullable();
+            $table->dateTime('data_date')->nullable();
+            $table->text('data_text')->nullable();
+            $table->boolean('status'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
